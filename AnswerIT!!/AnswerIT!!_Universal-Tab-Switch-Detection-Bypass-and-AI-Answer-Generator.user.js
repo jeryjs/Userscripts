970406404e21003da988dc46d0ee496e3d512e27
Jery Js <jery99961@gmail.com>
2025-06-23 18:04:33 +0530

    [AnswerIt] Increase button gap in popup header controls

--- AnswerIT!!/AnswerIT!!_Universal-Tab-Switch-Detection-Bypass-and-AI-Answer-Generator.user.js
+++ AnswerIT!!/AnswerIT!!_Universal-Tab-Switch-Detection-Bypass-and-AI-Answer-Generator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AnswerIT!! - Universal Tab Switch Detection Bypass and AI Answer Generator
 // @namespace    https://github.com/jeryjs
-// @version      3.14.0
+// @version      3.14.1
 // @description  Universal tab switch detection bypass and AI answer generator with popup interface
 // @author       Jery
 // @match        https://app.joinsuperset.com/assessments/*
@@ -255,6 +255,7 @@
 		#ai-popup-controls {
 			display: flex;
 			align-items: center;
+   			gap: 5px;
 		}
 
 		#ai-popup-controls > button {
